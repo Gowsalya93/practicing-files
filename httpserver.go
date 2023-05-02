627d122d8845269707772723081600dfe263258e
Gowsalya93 <130340537+Gowsalya93@users.noreply.github.com>
2023-05-02 10:54:21 +0530

    Add files via upload

--- httpserver.go
+++ httpserver.go
@@ -9,6 +9,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", version)
+	http.HandleFunc("/next", version1)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
@@ -17,4 +18,7 @@ func main() {
 
 func version(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "golang version 1.12")
+}
+func version1(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "golang version 2.0")
 }
